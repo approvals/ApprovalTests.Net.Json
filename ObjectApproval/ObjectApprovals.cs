10e9e835eb4f5c54fd357b7dea5eaf1e33b03fca
Llewellyn Falco <isidore@setgame.com>
2014-06-11 18:31:35 -0500

    Allow json formatted to be used by others

--- ObjectApproval/ObjectApprovals.cs
+++ ObjectApproval/ObjectApprovals.cs
@@ -3,18 +3,24 @@ using ApprovalTests;
 
 namespace ObjectApproval
 {
-    public static class ObjectApprover
-    {
-        public static void VerifyWithJson(object target)
-        {
-            VerifyWithJson(target, s => s);
-        }
+	public static class ObjectApprover
+	{
+		public static void VerifyWithJson(object target)
+		{
+			VerifyWithJson(target, s => s);
+		}
 
-        public static void VerifyWithJson(object target, Func<string,string> scrubber)
-        {
-            var serializeObject = SimpleJson.SerializeObject(target, new EnumSupportedStrategy());
-            var formatJson = serializeObject.FormatJson().FixNewLines();
-            Approvals.Verify(formatJson, s => scrubber(s).FixNewLines());
-        }
-    }
+		public static void VerifyWithJson(object target, Func<string, string> scrubber)
+		{
+			var formatJson = AsFormattedJson(target);
+			Approvals.Verify(formatJson, s => scrubber(s).FixNewLines());
+		}
+
+		public static string AsFormattedJson(object target)
+		{
+			var serializeObject = SimpleJson.SerializeObject(target, new EnumSupportedStrategy());
+			var formatJson = serializeObject.FormatJson().FixNewLines();
+			return formatJson;
+		}
+	}
 }
