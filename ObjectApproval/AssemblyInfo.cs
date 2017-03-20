898d334dd751a34f96d6e3d3fb543325e017e257
Simon Cropp <simon.cropp@gmail.com>
2017-03-20 21:53:59 +1100

    bump version

--- ObjectApproval/AssemblyInfo.cs
+++ ObjectApproval/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using ApprovalTests.Reporters;
 
 [assembly: AssemblyTitle("ObjectApproval")]
 [assembly: AssemblyProduct("ObjectApproval")]
-[assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0")]
 [assembly: UseReporter(typeof(ClipboardReporter), typeof(DiffReporter))]
