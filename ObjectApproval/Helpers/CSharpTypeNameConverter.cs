b543a98ee8e4ac69484b1c9a3c2841cb901c185e
Simon Cropp <simon.cropp@gmail.com>
2018-10-30 12:56:38 +1100

    better type name handling

---
+++ ObjectApproval/Helpers/CSharpTypeNameConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CSharp;
+
+namespace ObjectApproval
+{
+    public static class CSharpTypeNameConverter
+    {
+        static CSharpCodeProvider codeDomProvider = new CSharpCodeProvider();
+
+        public static string GetName(Type type)
+        {
+            //TODO: cache
+            var typeName = type.FullName.Replace(type.Namespace + ".", "");
+            var reference = new CodeTypeReference(typeName);
+            var name = codeDomProvider.GetTypeOutput(reference);
+            var list = new List<string>();
+            AllGenericArgumentNamespace(type, list);
+            foreach (var ns in list.Distinct())
+            {
+                name = name.Replace($"<{ns}.", "<");
+                name = name.Replace($", {ns}.", ", ");
+            }
+
+            return name;
+        }
+
+        static void AllGenericArgumentNamespace(Type type, List<string> list)
+        {
+            if (type.Namespace != null)
+            {
+                list.Add(type.Namespace);
+            }
+
+            var elementType = type.GetElementType();
+
+            if (elementType != null)
+            {
+                AllGenericArgumentNamespace(elementType,list);
+            }
+
+            foreach (var generic in type.GenericTypeArguments)
+            {
+                AllGenericArgumentNamespace(generic, list);
+            }
+        }
+    }
+}
--- ObjectApproval/Helpers/ShortNameBinder.cs
+++ ObjectApproval/Helpers/ShortNameBinder.cs
@@ -1,30 +1,19 @@
 ﻿using System;
-using System.CodeDom;
-using Microsoft.CSharp;
 using Newtonsoft.Json.Serialization;
 
 namespace ObjectApproval
 {
     public class ShortNameBinder : ISerializationBinder
     {
-        static CSharpCodeProvider codeDomProvider = new CSharpCodeProvider();
-
         public void BindToName(Type serializedType, out string assemblyName, out string typeName)
         {
             assemblyName = null;
-            typeName = GetOriginalName(serializedType);
+            typeName = CSharpTypeNameConverter.GetName(serializedType);
         }
 
         public Type BindToType(string assemblyName, string typeName)
         {
             throw new Exception();
-        }
-
-        static string GetOriginalName(Type type)
-        {
-            var typeName = type.FullName.Replace(type.Namespace + ".", "");
-            var reference = new CodeTypeReference(typeName);
-            return codeDomProvider.GetTypeOutput(reference);
         }
     }
 }
