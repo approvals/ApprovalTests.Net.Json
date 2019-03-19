df658daca9d3e51eadf4d7e272da4d5774e462c6
Simon Cropp <simon.cropp@gmail.com>
2019-03-19 14:07:47 +1100

    .

--- src/ObjectApproval/Scrubbers/DelegateConverter.cs
+++ src/ObjectApproval/Scrubbers/DelegateConverter.cs
@@ -11,16 +11,23 @@ namespace ObjectApproval
             {
                 return;
             }
-            var multicastDelegate = (Delegate)value;
+
+            var @delegate = (Delegate)value;
 
             writer.WriteStartObject();
             writer.WritePropertyName("Type");
-            writer.WriteValue(TypeNameConverter.GetName(multicastDelegate.GetType()));
+            writer.WriteValue(TypeNameConverter.GetName(@delegate.GetType()));
             writer.WritePropertyName("Target");
-            writer.WriteValue(TypeNameConverter.GetName(multicastDelegate.Method.DeclaringType));
+            writer.WriteValue(TypeNameConverter.GetName(@delegate.Method.DeclaringType));
             writer.WritePropertyName("Method");
-            writer.WriteValue(multicastDelegate.Method.ToString());
+            var s = @delegate.Method.ToString();
+            writer.WriteValue(CleanMethodName(s));
             writer.WriteEndObject();
+        }
+
+        public static string CleanMethodName(string s)
+        {
+            return s;
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
---
+++ src/Tests/DelegateConverterTests.cs
@@ -0,0 +1,12 @@
+﻿using ObjectApproval;
+using Xunit;
+
+public class DelegateConverterTests
+{
+    [Fact]
+    public void Simple()
+    {
+        var cleaned = DelegateConverter.CleanMethodName("Void <SetProperties>b__100_2(System.String, Message.Importance)");
+        Assert.Equal("Void SetProperties(System.String, Message.Importance", cleaned);
+    }
+}
