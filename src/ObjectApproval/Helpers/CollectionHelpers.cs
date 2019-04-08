12f1da11fa5cea5daa84be090351af90e8a95e31
Simon Cropp <simon.cropp@gmail.com>
2019-04-08 12:44:05 +1000

    split out CollectionHelpers

---
+++ src/ObjectApproval/Helpers/CollectionHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+static class CollectionHelpers
+{
+    public static bool IsCollection(this Type type)
+    {
+        Guard.AgainstNull(type, nameof(type));
+        return type.GetInterfaces()
+            .Any(x => x.IsGenericType &&
+                      x.GetGenericTypeDefinition() == typeof(ICollection<>));
+    }
+
+    public static IEnumerable GetCollection(this MemberInfo member, object instance)
+    {
+        // this value could be in a public field or public property
+        if (member is PropertyInfo propertyInfo)
+        {
+            return (IEnumerable) propertyInfo.GetValue(instance, null);
+        }
+
+        if (member is FieldInfo fieldInfo)
+        {
+            return (IEnumerable) fieldInfo.GetValue(instance);
+        }
+
+        throw new Exception($"No supported MemberType: {member.MemberType}");
+    }
+}
--- src/ObjectApproval/Helpers/ContractResolutionHelpers.cs
+++ src/ObjectApproval/Helpers/ContractResolutionHelpers.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using Newtonsoft.Json.Serialization;
 
 namespace ObjectApproval
 {
     public static class ContractResolutionHelpers
     {
-        static bool IsCollection(this Type type)
-        {
-            Guard.AgainstNull(type, nameof(type));
-            return type.GetInterfaces()
-                .Any(x => x.IsGenericType &&
-                          x.GetGenericTypeDefinition() == typeof(ICollection<>));
-        }
-
         public static void SkipEmptyCollections(this JsonProperty property, MemberInfo member)
         {
             Guard.AgainstNull(property, nameof(property));
@@ -33,7 +21,7 @@ namespace ObjectApproval
 
             property.ShouldSerialize = instance =>
             {
-                var collection = GetCollection(member, instance);
+                var collection = member.GetCollection(instance);
 
                 if (collection == null)
                 {
@@ -44,22 +32,6 @@ namespace ObjectApproval
                 // check to see if there is at least one item in the Enumerable
                 return collection.GetEnumerator().MoveNext();
             };
-        }
-
-        static IEnumerable GetCollection(MemberInfo member, object instance)
-        {
-            // this value could be in a public field or public property
-            if (member is PropertyInfo propertyInfo)
-            {
-                return (IEnumerable) propertyInfo.GetValue(instance, null);
-            }
-
-            if (member is FieldInfo fieldInfo)
-            {
-                return (IEnumerable) fieldInfo.GetValue(instance);
-            }
-
-            throw new Exception($"No supported MemberType: {member.MemberType}");
         }
     }
 }
