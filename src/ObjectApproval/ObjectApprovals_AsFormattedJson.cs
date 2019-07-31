bd047aad14e4e3011ab4927782561a945635d932
Simon Cropp <simon.cropp@gmail.com>
2019-07-31 10:46:12 +1000

    Create ObjectApprovals_AsFormattedJson.cs

---
+++ src/ObjectApproval/ObjectApprovals_AsFormattedJson.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ObjectApproval
+{
+    public static partial class ObjectApprover
+    {
+        public static string AsFormattedJson(object target, JsonSerializerSettings jsonSerializerSettings = null)
+        {
+            var serializer = GetJsonSerializer(jsonSerializerSettings);
+            var builder = new StringBuilder();
+            using (var stringWriter = new StringWriter(builder))
+            using (var writer = new JsonTextWriter(stringWriter))
+            {
+                writer.QuoteChar = '\'';
+                writer.QuoteName = false;
+                serializer.Serialize(writer, target);
+            }
+
+            builder.Replace(@"\\", @"\");
+            return builder.ToString();
+        }
+
+        static JsonSerializer GetJsonSerializer(JsonSerializerSettings jsonSerializerSettings)
+        {
+            if (jsonSerializerSettings == null)
+            {
+                return JsonSerializer.Create(SerializerBuilder.BuildSettings());
+            }
+
+            return JsonSerializer.Create(jsonSerializerSettings);
+        }
+    }
+}
